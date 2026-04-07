829820f0e27bad399cd6148af1ef4c5b1ab76d70
Learnmorecoding <akankshas2703@gmail.com>
2026-04-07 20:09:41 +0530

    new changes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/react-tailwind-portfolio/",
+ // base: "/react-tailwind-portfolio/",
+ /* server: {
+    open:true,
+    port:4001,
+  },*/
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
